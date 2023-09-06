7061c3f6021967cf6e1f029a4505453230524743
nearworld <artemismars2@outlook.com>
2023-09-06 20:31:56 +0900

    style: SearchBar 컴포넌트 마크업

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,24 +4,37 @@ import styled from 'styled-components';
 export const SearchBar = () => {
   return (
     <StyledSearchBar>
-      <StyledPlaceholder>
-        <ZoomIcon width={20} height={20} />
-        <span>질환명을 입력해주세요.</span>
-      </StyledPlaceholder>
-      <StyledButton></StyledButton>
+      <StyledInputBox>
+        <StyledPlaceholder>
+          <ZoomIcon width={20} height={20} />
+          <span>질환명을 입력해주세요.</span>
+        </StyledPlaceholder>
+        <StyledInput type='text' width='100%' />
+      </StyledInputBox>
+      <StyledButton>
+        <ZoomIcon width={20} height={20} fill='white' />
+      </StyledButton>
     </StyledSearchBar>
   );
 };
 
 const StyledSearchBar = styled.div`
   max-width: 490px;
-  padding: 20px 10px 20px 24px;
+  width: 100%;
+  display: flex;
+  align-items: center;
   border-radius: 42px;
   border: 2px solid white;
   background-color: white;
 `;
 
+const StyledInputBox = styled.div`
+  flex: 2;
+  padding: 20px 10px 20px 24px;
+`;
+
 const StyledPlaceholder = styled.div`
+  position: absolute;
   display: flex;
   gap: 12px;
   align-items: center;
@@ -30,9 +43,19 @@ const StyledPlaceholder = styled.div`
   cursor: text;
 `;
 
+const StyledInput = styled.input`
+  border: none;
+  padding-right: 20px;
+`;
+
 const StyledButton = styled.button`
   border-radius: 100%;
   width: 48px;
   height: 48px;
+  border: none;
+  cursor: pointer;
   background-color: #007be9;
+  display: grid;
+  place-items: center;
+  margin-right: 10px;
 `;
