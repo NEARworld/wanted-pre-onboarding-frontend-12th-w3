f197ba2423aaa402d1c6d5ff519f0bc4191cae3e
nearworld <artemismars2@outlook.com>
2023-09-06 20:06:22 +0900

    feat: SearchBar 컴포넌트에 placeholder 구현

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,29 @@
+import { ReactComponent as ZoomIcon } from 'assets/zoom.svg';
 import styled from 'styled-components';
 
 export const SearchBar = () => {
-  return <StyledInput type='text' />;
+  return (
+    <StyledSearchBar>
+      <StyledPlaceholder>
+        <ZoomIcon width={20} height={20} />
+        <span>질환명을 입력해주세요.</span>
+      </StyledPlaceholder>
+    </StyledSearchBar>
+  );
 };
 
-const StyledInput = styled.input`
+const StyledSearchBar = styled.div`
   max-width: 490px;
+  padding: 20px 10px 20px 24px;
+  border-radius: 42px;
+  border: 2px solid white;
+  background-color: white;
+`;
+
+const StyledPlaceholder = styled.div`
+  display: flex;
+  gap: 12px;
+  align-items: center;
+  color: #a7afb7;
+  font-size: 1.125rem;
 `;
